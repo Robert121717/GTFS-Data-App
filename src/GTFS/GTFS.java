b64e8daf8893f4ede791091759a40cdd0cacb333
atkinsonr <atkinsonr@msoe.edu>
2022-10-11 09:06:13 -0500

    remove toDecimal and mergeID methods.

--- src/GTFS/GTFS.java
+++ src/GTFS/GTFS.java
@@ -29,7 +29,7 @@ public class GTFS {
 	/**
 	 * This method represents the constructor for the GTFS class
 	 */
-	public GTFS(){
+	public GTFS() {
 		routes = new ArrayList<>();
 		stops = new ArrayList<>();
 		stopTimes = new ArrayList<>();
@@ -47,19 +47,19 @@ public class GTFS {
 	 * @param file is the file that is to be imported
 	 */
 	protected void importFile(File file) {
-		try(Scanner in = new Scanner(file)) {
+		try (Scanner in = new Scanner(file)) {
 
 			String header = in.nextLine();
 			header = header.toUpperCase();
 
-			if(header.contains("ROUTE_ID")) {
-				if(header.contains("TRIP_ID")) {
+			if (header.contains("ROUTE_ID")) {
+				if (header.contains("TRIP_ID")) {
 					importTrip(file);
 				} else {
 					importRoute(file);
 				}
-			} else if(header.contains("STOP_ID")) {
-				if(header.contains("TRIP_ID")) {
+			} else if (header.contains("STOP_ID")) {
+				if (header.contains("TRIP_ID")) {
 					importStopTime(file);
 				} else {
 					importStop(file);
@@ -67,51 +67,54 @@ public class GTFS {
 			} else {
 				throw new IllegalArgumentException();
 			}
-		} catch(NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			newAlert(AlertType.ERROR, "Error Dialog", "Unexpected Value", e.getMessage());
 
-		} catch(FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			newAlert(AlertType.ERROR, "Error Dialog", "File Not Found", e.getMessage());
 
-		} catch(IllegalArgumentException e) {
+		} catch (IllegalArgumentException e) {
 			newAlert(AlertType.ERROR, "Error Dialog", "Invalid Data", e.getMessage());
 		}
 	}
 
 	/**
 	 * This method puts a new route into the routes hashtable
-	 * @param file is the route file being added to hashtable
 	 *
+	 * @param file is the route file being added to hashtable
 	 */
 	protected void importRoute(File file) {
 		try (Scanner in = new Scanner(file)) {
 			in.nextLine();
 			importRoute(in.hasNextLine(), in);
 
-		} catch(NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			newAlert(AlertType.ERROR, "Error Dialog", "Unexpected Value", e.getMessage());
 
-		} catch(FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			newAlert(AlertType.ERROR, "Error Dialog", "File Not Found", e.getMessage());
 		}
 	}
 
 	private void importRoute(boolean hasLine, Scanner in) {
 		int lineCount = 0;
-		while(hasLine) {
+		while (hasLine) {
 			String line = in.nextLine();
-			if(lineCount < 3) {
+			if (lineCount < 3) {
 				lineCount++;
 				stringBuilder.append(line);
 			}
 			String[] parts = line.split(",");
 			Route route = new Route(parts[0]);
 
-			route.setAgencyID(parts[1]); route.setShortName(parts[2]);
-			route.setLongName(parts[3]); route.setRouteDesc(parts[4]);
-			route.setRouteType(parts[5]); route.setRouteURL(parts[6]);
+			route.setAgencyID(parts[1]);
+			route.setShortName(parts[2]);
+			route.setLongName(parts[3]);
+			route.setRouteDesc(parts[4]);
+			route.setRouteType(parts[5]);
+			route.setRouteURL(parts[6]);
 			route.setRouteColor(parts[7]);
-			if(parts.length == 9) {
+			if (parts.length == 9) {
 				route.setRouteTextColor(parts[8]);
 			} else {
 				route.setRouteTextColor("");
@@ -128,35 +131,36 @@ public class GTFS {
 
 	/**
 	 * This method puts a new stop into the stops hashtable
-	 * @param file represent the stop file being added to hashtable
 	 *
+	 * @param file represent the stop file being added to hashtable
 	 */
 	protected void importStop(File file) {
 		try (Scanner in = new Scanner(file)) {
 			in.nextLine();
 			importStop(in.hasNextLine(), in);
 
-		} catch(NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			newAlert(AlertType.ERROR, "Error Dialog", "Unexpected Value", e.getMessage());
 
-		} catch(FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			newAlert(AlertType.ERROR, "Error Dialog", "File Not Found", e.getMessage());
 		}
 	}
 
 	private void importStop(boolean hasLine, Scanner in) {
 		int lineCount = 0;
-		while(hasLine) {
+		while (hasLine) {
 			String line = in.nextLine();
 
-			if(lineCount < 3) {
+			if (lineCount < 3) {
 				lineCount++;
 				stringBuilder.append(line);
 			}
 			String[] parts = line.split(",");
 			Stop stop = new Stop(parts[0]);
 
-			stop.setStopName(parts[1]); stop.setStopDesc(parts[2]);
+			stop.setStopName(parts[1]);
+			stop.setStopDesc(parts[2]);
 			stop.setStopLat(Double.parseDouble(parts[3]));
 			stop.setStopLon(Double.parseDouble(parts[4]));
 
@@ -171,28 +175,28 @@ public class GTFS {
 
 	/**
 	 * This method puts a new StopTime into the StopTimes ArrayList
-	 * @param file represents the StopTime file being added to ArrayList
 	 *
+	 * @param file represents the StopTime file being added to ArrayList
 	 */
 	protected void importStopTime(File file) {
-		try(Scanner in = new Scanner(file)) {
+		try (Scanner in = new Scanner(file)) {
 			in.nextLine();
 			importStopTime(in.hasNextLine(), in);
 
-		} catch(NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			newAlert(AlertType.ERROR, "Error Dialog", "Unexpected Value", e.getMessage());
 
-		} catch(FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			newAlert(AlertType.ERROR, "Error Dialog", "File Not Found", e.getMessage());
 		}
 	}
 
 	private void importStopTime(boolean hasLine, Scanner in) {
 		int lineCount = 0;
-		while(hasLine) {
+		while (hasLine) {
 			String line = in.nextLine();
 
-			if(lineCount < 3) {
+			if (lineCount < 3) {
 				lineCount++;
 				stringBuilder.append(line);
 			}
@@ -200,9 +204,12 @@ public class GTFS {
 			String[] parts = line.split(",");
 			StopTime st = new StopTime(parts[3], parts[0]);
 
-			st.setArrivalTime(parts[1]); st.setDepartureTime(parts[2]);
-			st.setStopSequence(parts[4]); st.setStopHeadSign(parts[5]);
-			st.setPickUpType(parts[6]); st.setDropOffType(parts[7]);
+			st.setArrivalTime(parts[1]);
+			st.setDepartureTime(parts[2]);
+			st.setStopSequence(parts[4]);
+			st.setStopHeadSign(parts[5]);
+			st.setPickUpType(parts[6]);
+			st.setDropOffType(parts[7]);
 
 			stopTimes.add(st);
 			if (!in.hasNextLine()) {
@@ -215,28 +222,28 @@ public class GTFS {
 
 	/**
 	 * This method puts a new trip into the trips ArrayList
-	 * @param file represents the trip file being added to ArrayList
 	 *
+	 * @param file represents the trip file being added to ArrayList
 	 */
 	protected void importTrip(File file) {
-		try(Scanner in = new Scanner(file)) {
+		try (Scanner in = new Scanner(file)) {
 			in.nextLine();
 			importTrip(in.hasNextLine(), in);
 
-		} catch(NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			newAlert(AlertType.ERROR, "Error Dialog", "Unexpected Value", e.getMessage());
 
-		} catch(FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			newAlert(AlertType.ERROR, "Error Dialog", "File Not Found", e.getMessage());
 		}
 	}
 
 	protected void importTrip(boolean hasLine, Scanner in) {
 		int lineCount = 0;
-		while(hasLine) {
+		while (hasLine) {
 			String line = in.nextLine();
 
-			if(lineCount < 3) {
+			if (lineCount < 3) {
 				lineCount++;
 				stringBuilder.append(line);
 			}
@@ -261,13 +268,14 @@ public class GTFS {
 
 	/**
 	 * search a stop and returns the number of trips that have the stop.
+	 *
 	 * @param stopId - searched stop
 	 * @return returns the number of trips that contain the stop
 	 */
 	public int numTripsWithStop(String stopId) {
 		int numTrips = 0;
-		for (StopTime stopTime: stopTimes) {
-			if(stopTime.hasStop(stopId)) {
+		for (StopTime stopTime : stopTimes) {
+			if (stopTime.hasStop(stopId)) {
 				numTrips++;
 			}
 		}
@@ -276,37 +284,47 @@ public class GTFS {
 
 	/**
 	 * NOT IMPLEMENTED YET
+	 *
 	 * @param string- not implemented yet
 	 */
 	public boolean export(String string) {
 		return false;
 	}
 
-	/** NOT IMPLEMENTED
+	/**
+	 * NOT IMPLEMENTED
 	 * Searches for a stop, given the stopID. Returns the Stop
+	 *
 	 * @param stopId- ID turned into ascii decimal used as a key for each stop.
 	 */
 	public Stop searchStopId(String stopId) {
 		return null;
 	}
 
-	/** NOT IMPLEMENTED
+	/**
+	 * NOT IMPLEMENTED
 	 * Searches for a route, given the routeId. Returns the route
+	 *
 	 * @param routeId- ID turned into ascii decimal used as a key for each stop
 	 */
 	public Route searchRouteId(BigInteger routeId) {
 		return null;
 	}
 
-	/** NOT IMPLEMENTED
+	/**
+	 * NOT IMPLEMENTED
 	 * Searches for a trip, given the tripId. Returns the trip
+	 *
 	 * @param tripId- ID turned into ascii decimal used as a key for each trip
 	 */
 	public Trip searchTrips(BigInteger tripId) {
 		return null;
 	}
-	/** NOT IMPLEMENTED
+
+	/**
+	 * NOT IMPLEMENTED
 	 * Searches for a stop time, given the stopTimeId. Returns the stop time.
+	 *
 	 * @param stopTimeId- ID turned into ascii decimal used as a key for each stop time
 	 */
 	public StopTime searchStopTimes(BigInteger stopTimeId) {
@@ -317,34 +335,4 @@ public class GTFS {
 		return lastAdded;
 	}
 
-	/** DEPRICATED
-	 * Converts each ASCII character in the ID to its decimal representation and appends it to an integer.
-	 *
-	 * @param id The object's ID as a String.
-	 * @return The ID as an appended integer. This ID will represent an attribute of the relative class,
-	 * but may not be the ID be used when storing this object in a hash table.
-	 */
-	protected static BigInteger toDecimal(String id) {
-		byte[] idBytes = id.getBytes(StandardCharsets.US_ASCII);
-
-		StringBuilder idByteString = new StringBuilder();
-		for (byte idByte : idBytes) {
-			idByteString.append(idByte);
-		}
-
-		return new BigInteger(idByteString.toString());
-	}
-
-	/** DEPRICATED
-	 * Appends one ID onto another. This method should only be used when both IDs correspond to a single object.
-	 * @param v1i Integer representation of the first ID.
-	 * @param v2i Integer representation of the second ID.
-	 * @return A single ID to be used when storing this object in a hash table,
-	 * where the first IDs value comes before the second IDs value (such that hashId: [v1i][v2i]).
-	 */
-	protected static BigInteger mergeIDs(BigInteger v1i, BigInteger v2i) {
-		String v1 = String.valueOf(v1i), v2 = String.valueOf(v2i);
-
-		return new BigInteger(v1 + v2);
-	}
 }
