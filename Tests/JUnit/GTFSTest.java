9dfbf431b0e664489579dc5754d8640188abcc18
nairac <nairac@msoe.edu>
2022-10-12 16:12:59 -0500

    Added Test Class and Methods

---
+++ Tests/JUnit/GTFSTest.java
@@ -0,0 +1,44 @@
+package JUnit;
+
+class GTFSTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void verifyRouteHeader() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void verifyStopHeader() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void verifyTripHeader() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void verifyStopTimeHeader() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validateRouteData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validateStopData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validateTripData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validateStopTimeData() {
+    }
+}
