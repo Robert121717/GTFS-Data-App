ac63a821ab4a01c5e3d27716a4face18bb912980
nairac <nairac@msoe.edu>
2022-10-12 20:34:48 -0500

    Fixed Junit problem

--- Tests/JUnit/GTFSTest.java
+++ Tests/JUnit/GTFSTest.java
@@ -1,13 +1,17 @@
 package JUnit;
 
+import GTFS.GTFS;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GTFSTest {
 
+    private GTFS gtfs;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
-
+        gtfs = new GTFS();
     }
 
     @org.junit.jupiter.api.AfterEach
@@ -35,37 +39,37 @@ class GTFSTest {
 
         String trueDataString = "12,MCTS,12,Teutonia-Hampton,,3,,008345,";
         String[] trueRouteDataTest = trueDataString.split(",");
-        assertTrue("Data was true",validateRouteData(trueRouteDataTest));
+        assertTrue(gtfs.validateRouteData(trueRouteDataTest));
 
 
         String shortDataString = "12,MCTS,12,";
         String[]  falseShortRouteDataTest = shortDataString.split(",");
-        assertFalse(validateRouteData(falseShortRouteDataTest));
+        assertFalse(gtfs.validateRouteData(falseShortRouteDataTest));
 
         String longDataString = "12,MCTS,12,Teutonia-Hampton,,3,,008345,12,MCTS,12,Teutonia-Hampton,,3,,008345,";
         String[]  falseLongRouteDataTest = longDataString.split(",");
-        assertFalse(validateRouteData(falseLongRouteDataTest));
+        assertFalse(gtfs.validateRouteData(falseLongRouteDataTest));
 
         String missingRouteTypeDataString = ",MCTS,12,Teutonia-Hampton,,3,,008345,";
         String[]  missingRouteTypeDataTest = missingRouteTypeDataString.split(",");
-        assertFalse(validateRouteData(missingRouteTypeDataTest));
+        assertFalse(gtfs.validateRouteData(missingRouteTypeDataTest));
 
 
         String missingRouteColorDataString = "12,MCTS,12,Teutonia-Hampton,,3,,,";
         String[]  missingRouteColorDataTest = missingRouteColorDataString.split(",");
-        assertFalse(validateRouteData(missingRouteColorDataTest););
+        assertFalse(gtfs.validateRouteData(missingRouteColorDataTest));
 
         String invalidRouteColorDataString = "12,MCTS,12,Teutonia-Hampton,,3,,ZZZZZZ,";
         String[]  invalidRouteDataTest = invalidRouteColorDataString.split(",");
-        assertFalse(validateRouteData(invalidRouteDataTest));
+        assertFalse(gtfs.validateRouteData(invalidRouteDataTest));
 
-        String validRouteURLDataString = "12,MCTS,12,Teutonia-Hampton,,3,http://www.w3.org/albert/bertram/marie-claude,ZZZZZZ,";
+        String validRouteURLDataString = "12,MCTS,12,Teutonia-Hampton,,3,http://www.w3.org/albert/bertram/marie-claude,008345,";
         String[]  validRouteURLDataTest = validRouteURLDataString.split(",");
-        assertTrue(validateRouteData(validRouteURLDataTest));
+        assertTrue(gtfs.validateRouteData(validRouteURLDataTest));
 
-        String invalidRouteURLDataString = "12,MCTS,12,Teutonia-Hampton,,3,fxqn:/us/va/reston/cnri/ietf/24/asdf%*.fred,ZZZZZZ,";
+        String invalidRouteURLDataString = "12,MCTS,12,Teutonia-Hampton,,3,fxqn:/us/va/reston/cnri/ietf/24/asdf%*.fred,008345,";
         String[]  invalidRouteURLDataTest = invalidRouteURLDataString.split(",");
-        assertTrue(validateRouteData(invalidRouteURLDataTest););
+        assertTrue(gtfs.validateRouteData(invalidRouteURLDataTest));
     }
 
     @org.junit.jupiter.api.Test
